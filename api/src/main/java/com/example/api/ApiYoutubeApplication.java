c9c90e9a9e164dc337e1d0defb0c0ed01deff2fd
killislac <kamilylacerda1@gmail.com>
2024-09-16 09:58:52 -0300

    Commit before normalizing line endings

---
+++ api/src/main/java/com/example/api/ApiYoutubeApplication.java
@@ -0,0 +1,13 @@
+package com.example.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiYoutubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiYoutubeApplication.class, args);
+	}
+
+}
---
+++ api/src/main/java/com/example/api/controller/ApiController.java
@@ -0,0 +1,40 @@
+package com.example.api.controller;
+
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class ApiController {
+
+    private List<String> tasks = new ArrayList<>();
+
+    private ObjectMapper objectMapper;
+
+    public ApiController(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @GetMapping(path="/tasks")
+    public ResponseEntity<String> ListTasks() throws JsonProcessingException {
+        return ResponseEntity.ok(objectMapper.writeValueAsString(tasks));
+    }
+
+    @PostMapping(path = "/tasks")
+    public ResponseEntity<Void> createTask(@RequestBody String task) {
+        tasks.add(task);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping(path = "/tasks")
+    public ResponseEntity<Void> clearTasks(){
+        tasks = new ArrayList<>();
+        return ResponseEntity.ok().build();
+    }
+}
